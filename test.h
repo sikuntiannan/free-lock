c2bb4aa127a7fd3bba127caadd89179c8a2c3dc5
邓政 <dengzheng2008@qq.com>
2021-06-16 18:07:50 +0800

    Update test.h
    std::thread 没有detach（）时，父线程结束将导致未结束的子线程崩溃。所以添加detach（）。增加等待，模拟实际执行。

--- test.h
+++ test.h
@@ -6,7 +6,7 @@ using namespace swd;
 
 std::vector<S_Lock> lock_array;
 
-
+std::atomic<int> thread_number = 100;
 
 void sun ()
 {
@@ -17,50 +17,71 @@ void sun ()
 		int n = v % 100;
 		Lock_Guard te_l (lock_array[n]);
 
-		int v = std::rand ();
-		if (v & 0x00001)
+		v = std::rand ();
+		if ((thread_number--)>0 && (v & 0x00001))
 		{
 			Thread t (sun);
-
-			int v = std::rand ();
+			v = std::rand ();
 			if (v & 0x00001)
 			{
 				t.join ();
+			}
+			else
+			{
+				t.detach ();
 			}
 		}
 	}
 	else
 	{
-		int v = std::rand ();
-		if (v & 0x00001)
+		v = std::rand ();
+		if ((thread_number--)>0 && (v & 0x00001))
 		{
 			Thread t (sun);
-
-			int v = std::rand ();
+			v = std::rand ();
 			if (v & 0x00001)
 			{
 				t.join ();
 			}
+			else
+			{
+				t.detach ();
+			}
 		}
 	}
-
+	v = std::rand ();
+	while((v & 0x00001))
+	{
+		v = std::rand ();
+		_WAIT_;
+	}
 }
 
 void lock_test()
 {
-	lock_array.resize (100);
-	int counter = 10000;
-	while (counter--)
+	try
 	{
-		std::vector<Thread> t_te_l;
-		for (size_t i = 0; i < 50; i++)
+		lock_array.resize (100);
+		int counter = 10000;
+		while (counter--)
 		{
-			t_te_l.emplace_back (sun);
+			std::vector<Thread> t_te_l;
+			for (size_t i = 0; i < 4; i++)
+			{
+				t_te_l.emplace_back (sun);
+			}
+			for (auto &var : t_te_l)
+			{
+				var.join ();
+			}
+			t_te_l.clear ();
+			thread_number = 100;
+			std::cout << "第" << 10000 - counter << "次完成" << std::endl;
 		}
-		for (auto &var : t_te_l)
-		{
-			var.join ();
-		}
-		std::cout << "第"<< 10000 -counter<<"次完成"<< std::endl;
 	}
+	catch (const std::exception&err)
+	{
+		std::cout << err.what () << std::endl;
+	}
+	
 }
